874b558a278d698ca1e9a25c715947f8a371d114
unknown <arianne.domka@gmail.com>
2021-07-13 18:22:50 -0400

    login/register endpoints, arianne's branch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,83 @@
 const router = require("express").Router();
 const { UserModel } = require("../models");
+const { UniqueContraintError } = require("sequelize/lib/errors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res) => {
+
+    let { username, email, password } = req.body.user;
+    try{
+        const User = await UserModel.create({
+            username,
+            email,
+            password: bcrypt.hashSync(passwordhash, 13),
+        });
+
+        let token = jwt.sign({id: User.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+    
+        res.status(201).json({
+            message: "User successfully registered",
+            user: User,
+            sessionToken: token
+        });
+    } catch (err) {
+        if (err instanceof UniqueContraintError) {
+            res.status(409).json({
+                message: "Email or username already in use",
+            });
+        } else {
+            res.status(500).json({
+                message: "Failed to register user",
+            });
+        }
+    }
+    
+});
+
+router.post("/login", async (req, res) => {
+
+    let { email, password } = req.body.user;
+    try{   
+        const loginUser = await UserModel.findOne({
+            where: {
+                email: email,
+            },
+         });
+         if (loginUser) {
+
+            let passwordComparison = await bcrypt.compare(passwordhash, loginUser.passwordhash);
+
+            if (passwordComparison) {
+
+            let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60* 24});
+
+            res.status(200).json({
+                message: "User successfully logged in!",
+                user: loginUser,
+                sessionToken: token
+            });
+        } else {
+            res.status(401).json({
+                message: "Login failed",
+            });
+        }
+    } else {
+        res.status(401).json({
+            message: "Incorrect username or password"
+        });
+    }
+    } catch (err) {
+        
+            res.status(500).json({
+                message: "Failed to login user",
+            });
+        
+        
+        }
+    
+});
+
 
 
 /*
