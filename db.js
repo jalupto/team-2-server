da75972e99843ae6f42d26d6a33a6000f40b67ae
Jared Upton <jalupto@gmail.com>
2021-07-13 20:45:41 -0400

    all endpoints now working

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-    "postgres://postgres:78ec3f8353464c4d9f245c6c85f92814@localhost:5432/team-2-server"
+    "postgres://postgres:69f3e71fc61e49fdb0dd226d2981fcc6@localhost:5432/team-2-server"
 );
 
 module.exports = sequelize;
-
