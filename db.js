f3e67150bff883748daf67c271cfc0b2335be5cf
jalupto <80435311+jalupto@users.noreply.github.com>
2021-07-14 23:35:43 -0400

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize(
-    "postgres://postgres:69f3e71fc61e49fdb0dd226d2981fcc6@localhost:5432/team-2-server"
-);
-
-module.exports = sequelize;
